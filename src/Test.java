2412b0d8a7786b9c3bc849ef1da34474571b9c3d
Storm <stormkroonen@hotmail.nl>
2023-09-14 16:04:11 +0200

    added first testing method

--- src/Test.java
+++ src/Test.java
@@ -9,7 +9,7 @@ public class Test {
 
         // Reader to wait for user input
         Scanner reader = new Scanner(System.in);
-
+        System.out.println("initializing tester");
         // Test array's for calculated methods
         int[] testBarometer = {0, 30000, -30000, 50000, -50000};
         int[] testTemperature = {0, 700, 850, 1000, -700, -1000, 50000, -50000};
@@ -21,45 +21,52 @@ public class Test {
 
         // For-loop testArray's
         // Barometer test
+        System.out.println("Barometer test:");
         for (int i = 0; i < testBarometer.length - 1; i++) {
             double barometer = ValueConverter.barometer((short) testBarometer[i]);
-            System.out.println(barometer + ", index:" + testBarometer[i]);
+            System.out.println("Output: "+barometer + ", Input:" + testBarometer[i]);
         }
         reader.next();
         // Temperature test
+        System.out.println("Temperature test:");
         for (int i = 0; i < testTemperature.length - 1; i++) {
             double temperature = ValueConverter.temperature((short) testTemperature[i]);
-            System.out.println(temperature + ", index:" + testTemperature[i]);
+            System.out.println("Output: "+temperature + ", Input:" + testTemperature[i]);
         }
         reader.next();
         // Windspeed test
+        System.out.println("WindSpeed test:");
         for (int i = 0; i < testWindspeed.length - 1; i++) {
-            double windSpeed = ValueConverter.windspeed((short) testWindspeed[i]);
-            System.out.println(windSpeed + ", index:" + testWindspeed[i]);
+            double windSpeed = ValueConverter.windSpeed((short) testWindspeed[i]);
+            System.out.println("Output: "+windSpeed + ", Input:" + testWindspeed[i]);
         }
         reader.next();
         // Rainrate test
+        System.out.println("RainRate test:");
         for (int i = 0; i < testRainRate.length - 1; i++) {
-            double rainRate = ValueConverter.rainrate((short) testRainRate[i]);
-            System.out.println(rainRate + ", index:" + testRainRate[i]);
+            double rainRate = ValueConverter.rainRate((short) testRainRate[i]);
+            System.out.println("Output: "+rainRate + ", Input:" + testRainRate[i]);
         }
         reader.next();
         // UvIndex test
+        System.out.println("UVIndex test:");
         for (int i = 0; i < testUvIndex.length - 1; i++) {
             double UvIndex = ValueConverter.uvindex((short) testUvIndex[i]);
-            System.out.println(UvIndex + ", index:" + testUvIndex[i]);
+            System.out.println("Output: "+UvIndex + ", Input:" + testUvIndex[i]);
         }
         reader.next();
         // BetteryLevel test
+        System.out.println("BatteryLevel test:");
         for (int i = 0; i < testBatteryLevel.length - 1; i++) {
-            double batteryLevel = ValueConverter.batterylevel((short) testBatteryLevel[i]);
-            System.out.println(batteryLevel + ", index:" + testBatteryLevel[i]);
+            double batteryLevel = ValueConverter.batteryLevel((short) testBatteryLevel[i]);
+            System.out.println("Output: "+batteryLevel + ", Input:" + testBatteryLevel[i]);
         }
         reader.next();
         // Humidity test
+        System.out.println("Humidity test:");
         for (int i = 0; i < testHumidity.length - 1; i++) {
             double humidity = ValueConverter.humidity((short) testHumidity[i]);
-            System.out.println(humidity + ", index:" + testHumidity[i]);
+            System.out.println("Output: "+humidity + ", Input:" + testHumidity[i]);
         }
         reader.next();
     }
