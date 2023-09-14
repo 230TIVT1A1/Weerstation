8b8b4bd57649e3b1fb5cf8fd507783dc9077edfa
boomkip <mpl.groeneveld@gmail.com>
2023-09-14 19:21:19 +0200

    Added checks and new logic to ValueConverter.java

--- src/ValueConverter.java
+++ src/ValueConverter.java
@@ -3,27 +3,30 @@ import java.math.BigDecimal;
 public class ValueConverter {
 
     public static void main(String[] Args) {
-        System.out.println("Luchtdruk: " + airPressure((short) 30127));
-        System.out.println("Temperatuur: " + temperature((short) 795));
+        System.out.println("Luchtdruk: " + airPressure((short) 30500));
+        System.out.println("Temperatuur: " + temperature((short) 775));
         System.out.println("Luchtvochtigheid: " + humidity((short) 65));
-        System.out.println("Windsnelheid: " + windSpeed((short) 34));
+        System.out.println("Windsnelheid: " + windSpeed((short) 25));
         System.out.println("Windrichting: " + windDirection((short) 72));
         System.out.println("regensnelheid " + rainRate((short) 300));
         System.out.println("batterylevel " + batteryLevel((short) 193));
-        System.out.println("Sunrise: "  + sunrise((short) 2202));
-        System.out.println("sunset: "  + sunrise((short) 2202));
+        System.out.println("Sunrise: "  + sunrise((short) 526));
+        System.out.println("Sunset: "  + sunset((short) 2202));
     }
 
     public static double airPressure(short rawValue) {
-        double press = 0;
-        press = (rawValue / 1000) * 33.863889532610884;
-        Double result = new BigDecimal(press).setScale(1, BigDecimal.ROUND_HALF_UP).doubleValue();
-        return result;
+        if (rawValue > 0) {
+            double press = (rawValue / 1000.0) * 33.863889532610884;
+            return Math.round(press * 10.0) / 10.0;
+        }
+        return 0;
     }
 
     public static double temperature(short rawValue) {
-        double calc = 0;
-        calc = Math.round(((rawValue / 10) -32 ) * 0.55555555555);
+        if (rawValue < -4596 || rawValue > 10000) {
+            throw new IllegalArgumentException("Input rawValue is out of valid range for temperature.");
+        }
+        double calc = Math.round(((rawValue / 10.0) - 32) * 5.0/9.0 * 10.0) / 10.0;
         return calc;
     }
 
@@ -33,7 +36,13 @@ public class ValueConverter {
     }
 
     public static double windSpeed(short rawValue) {
+        if (rawValue < 0) {
+            throw new IllegalArgumentException("Wind speed cannot be negative.");
+        }
         double speed = rawValue * 1.609344;
+        if (speed > 200.0) {
+            throw new IllegalArgumentException("Wind speed value is unrealistically high.");
+        }
         Double result = new BigDecimal(speed).setScale(1, BigDecimal.ROUND_HALF_UP).doubleValue();
         return result;
     }
@@ -44,20 +53,24 @@ public class ValueConverter {
     }
 
     public static double rainRate(short rawValue) {
-        double rate = (rawValue / 100) * 25.40;
+        double rate = (rawValue / 100.0) * 25.4;
         Double result = new BigDecimal(rate).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
         return result;
-
     }
 
     public static double uvIndex(short rawValue) {
-        double uv = rawValue / 10;
+        if (rawValue < 0) {
+            throw new IllegalArgumentException("UV index value cannot be negative.");
+        }
+        double uv = rawValue / 10.0;
         return uv;
-
     }
 
     public static double batteryLevel(short rawValue) {
-        double level = 1.0*((rawValue * 300) / 512) / 100;
+        if (rawValue < 0 || rawValue > 512) {
+            throw new IllegalArgumentException("Battery level value is out of valid range.");
+        }
+        double level = 1.0 * ((rawValue * 300) / 512) / 100;
         return level;
     }
 
@@ -67,7 +80,7 @@ public class ValueConverter {
         int hours = inputTime / 100;
         int minutes = inputTime % 100;
 
-        String formattedTime = String.format(" %d:%02d", hours, minutes);
+        String formattedTime = String.format("%d:%02d", hours, minutes);
         return formattedTime;
     }
 
@@ -77,7 +90,7 @@ public class ValueConverter {
         int hours = inputTime / 100;
         int minutes = inputTime % 100;
 
-        String formattedTime = String.format(" %d:%02d", hours, minutes);
+        String formattedTime = String.format("%d:%02d", hours, minutes);
         return formattedTime;
     }
 }
