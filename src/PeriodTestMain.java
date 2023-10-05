60450d742687884f8f9e38625df2c69dc93a5786
Doppie <eriksszon@gmail.com>
2023-10-05 12:01:02 +0200

    made a methode for dewpoint

--- src/PeriodTestMain.java
+++ src/PeriodTestMain.java
@@ -3,7 +3,7 @@ public class PeriodTestMain {
 
 		Period period = new Period(7); // last week
 
-		System.out.println("The average temperature of last week was " + period.getAverageOutsideTemperature());
+		System.out.println("The age temperature of last week was " + period.getAverageOutsideTemperature());
 
 
 	}
