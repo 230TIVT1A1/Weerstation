66ad7e502e9f31ae8079b7adde298df678020d31
Daan Stander <daanstander@hotmail.nl>
2023-10-11 09:35:24 +0200

    Added individual assignment: i

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,104 @@
-
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        RawMeasurement rawMeasurement = DatabaseConnection.getMostRecentMeasurement();
-        Measurement measurement = new Measurement(rawMeasurement);
-        System.out.println(measurement.getOutsideTemp());
-        System.out.println(measurement.getOutsideHum());
-        System.out.println(measurement.getInsideTemp());
-        IO.init();
-        GUI.clearSegment();
-        GUI.segmentNumber(measurement.getOutsideTemp(),0,"00.00");
-        GUI.segmentNumber(measurement.getOutsideHum(),1,"00");
-        GUI.segmentNumber(measurement.getInsideTemp(),2,"0.0");
+//        RawMeasurement rawMeasurement = DatabaseConnection.getMostRecentMeasurement();
+//        Measurement measurement = new Measurement(rawMeasurement);
+//        System.out.println(measurement.getOutsideTemp());
+//        System.out.println(measurement.getOutsideHum());
+//        System.out.println(measurement.getInsideTemp());
+//        IO.init();
+//        GUI.clearSegment();
+//        GUI.segmentNumber(measurement.getOutsideTemp(),0,"00.00");
+//        GUI.segmentNumber(measurement.getOutsideHum(),1,"00");
+//        GUI.segmentNumber(measurement.getInsideTemp(),2,"0.0");
+        ArrayList<RawMeasurement> rawMeasurements = DatabaseConnection.getMeasurementsLastHour();
+        ArrayList<Measurement> measurements = new ArrayList<>();
+        Period period = new Period();
+        for (RawMeasurement rawMeasurement : rawMeasurements) {
+            Measurement measurement = new Measurement(rawMeasurement);
+            measurements.add(measurement);
+        }
+        /**
+         Mediaan
+         */
+        System.out.println("Mediaan Binnentemperatuur: " + period.getInsideTempMedian(measurements));
+        System.out.println("Mediaan Buitentemperatuur: " + period.getOutsideTempMedian(measurements));
+        System.out.println("Mediaan Barometer: " + period.getBarometerMedian(measurements));
+        System.out.println("Mediaan Luchtvochtigheid Buiten: " + period.getOutsideHumidityMedian(measurements));
+
+        System.out.println("---------------------------------------------");
+        /**
+         Laagste
+         */
+        System.out.println("Laagste Binnentemperatuur: " + period.getLowestInsideTemp(measurements));
+        System.out.println("Laagste Buitentemperatuur: " + period.getLowestOutsideTemp(measurements));
+        System.out.println("Laagste Barometer: " + period.getLowestBarometer(measurements));
+        System.out.println("Laagste Luchtvochtigheid Buiten: " + period.getLowestOutsideHumidity(measurements));
+
+        System.out.println("---------------------------------------------");
+        /**
+         Hoogste
+         */
+        System.out.println("Hoogste Binnentemperatuur: " + period.getHighestInsideTemp(measurements));
+        System.out.println("Hoogste Buitentemperatuur: " + period.getHighestOutsideTemp(measurements));
+        System.out.println("Hoogste Barometer: " + period.getHighestBarometer(measurements));
+        System.out.println("Hoogste Luchtvochtigheid Buiten: " + period.getHighestOutsideHumidity(measurements));
+
+        System.out.println("---------------------------------------------");
+        /**
+         Gemiddelde
+         */
+        System.out.println("Gemiddelde Binnentemperatuur: " + period.getAverageInsideTemp(measurements));
+        System.out.println("Gemiddelde Buitentemperatuur: " + period.getAverageOutsideTemp(measurements));
+        System.out.println("Gemiddelde Barometer: " + period.getAverageBarometer(measurements));
+        System.out.println("Gemiddelde Luchtvochtigheid Buiten: " + period.getAverageOutsideHumidity(measurements));
+
+        System.out.println("---------------------------------------------");
+        /**
+         Mediaan
+         */
+        System.out.println("Mediaan Binnentemperatuur: " + period.getInsideTempMedian(measurements));
+        System.out.println("Mediaan Buitentemperatuur: " + period.getOutsideTempMedian(measurements));
+        System.out.println("Mediaan Barometer: " + period.getBarometerMedian(measurements));
+        System.out.println("Mediaan Luchtvochtigheid Buiten: " + period.getOutsideHumidityMedian(measurements));
+
+        System.out.println("---------------------------------------------");
+
+        /**
+         Modus
+         */
+        System.out.println("Mode Binnentemperatuur: " + period.getInsideTempMode(measurements));
+        System.out.println("Mode Buitentemperatuur: " + period.getOutsideTempMode(measurements));
+        System.out.println("Mode Barometer: " + period.getBarometerMode(measurements));
+        System.out.println("Mode Luchtvochtigheid Buiten: " + period.getOutsideHumidityMode(measurements));
+
+        System.out.println("---------------------------------------------");
+
+        /**
+         Standaardafwijking
+         */
+        System.out.println("Standaardafwijking Binnentemperatuur: " + period.getInsideTempSD(measurements));
+        System.out.println("Standaardafwijking Buitentemperatuur: " + period.getOutsideTempSD(measurements));
+        System.out.println("Standaardafwijking Barometer: " + period.getBarometerSD(measurements));
+        System.out.println("Standaardafwijking Luchtvochtigheid Buiten: " + period.getOutsideHumiditySD(measurements));
+
+        /**
+         Individual Assignment (Daan)
+         Date of biggest difference between the WindChill and OutsideTemp
+         */
+        // Search back until 90 days ago
+        int daysBack = 90;
+        // Calculate period
+        LocalDate now = LocalDate.now();
+        LocalDate before = LocalDate.now().minusDays(daysBack);
+        System.out.println("Begin of period: " + now.minusDays(daysBack));
+        System.out.println("End of period: " + now);
+
+        Period p = new Period();
+        System.out.println("Biggest difference between the WindChill and OutsideTemp: " + p.getDiffWindChillAndOutsideTemp(before, now));
+        System.out.println("Date when this happend: " + p.getDateBiggestDiff(before, now));
     }
 }
--- src/Period.java
+++ src/Period.java
@@ -1,6 +1,9 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.time.*;
 import java.time.temporal.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * A class to contain a period of time
@@ -109,7 +112,7 @@ public class Period {
 		ArrayList<Measurement> measurements = getMeasurements();
 
 		//calculate average outside temperature and return it
-		return measurements.get(0).getTemperature();
+		return measurements.get(0).getOutsideTemp();
 	}
 
 	/**
@@ -129,6 +132,413 @@ public class Period {
 	/**
 	 * Todo more methods
 	 */
+//	public double getBarometerLowestValue(){
+//		ArrayList<Measurement> measurements = getMeasurements(this.beginPeriod, this.endPeriod);
+//		// Realistic values
+//		double prevBarometer = 1025;
+//		double lowestValue = 1025;
+//		for (Measurement measurement : measurements){
+//			double barometer = measurement.getBarometer();
+//			if (barometer < prevBarometer && barometer < lowestValue){
+//				lowestValue = barometer;
+//			} else if (prevBarometer < barometer && prevBarometer < lowestValue){
+//				lowestValue = prevBarometer;
+//			}
+//			prevBarometer = measurement.getBarometer();
+//		}
+//		return lowestValue;
+//	}
+//	public double getInsideTempLowestValue(){
+//		ArrayList<Measurement> measurements = getMeasurements(this.beginPeriod, this.endPeriod);
+//		// Realistic values
+//		double prevTemp = 1025;
+//		double lowestValue = ;
+//		for (Measurement measurement1 : measurements){
+//			for (Measurement measurement2 : measurements){
+//				double barometer = measurement1.getBarometer();
+//				if (barometer < prevBarometer && barometer < lowestValue){
+//					lowestValue = barometer;
+//				} else if (prevBarometer < barometer && prevBarometer < lowestValue){
+//					lowestValue = prevBarometer;
+//				}
+//				prevBarometer = measurement2.getBarometer();
+//			}
+//		}
+//		return lowestValue;
+//	}
+	public Double getLowestInsideTemp(ArrayList<Measurement> measurements) {
+		Measurement first = measurements.get(0);
+		double lowest = first.getInsideTemp();
+		for (Measurement measurement : measurements) {
+			if (measurement.getInsideTemp() < lowest) {
+				lowest = measurement.getInsideTemp();
+			}
+		}
+		return lowest;
+	}
+
+	public Double getHighestInsideTemp(ArrayList<Measurement> measurements) {
+		Measurement first = measurements.get(0);
+		double highest = first.getInsideTemp();
+		for (Measurement measurement : measurements) {
+			if (measurement.getInsideTemp() > highest) {
+				highest = measurement.getInsideTemp();
+			}
+		}
+		return highest;
+	}
+
+	public Double getAverageInsideTemp(ArrayList<Measurement> measurements) {
+		double sum = 0;
+		for (Measurement measurement : measurements) {
+			sum += measurement.getInsideTemp();
+		}
+		return sum / measurements.size();
+	}
+
+	public Double getInsideTempMedian(ArrayList<Measurement> measurements) {
+		ArrayList<Double> insideTemps = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			insideTemps.add(measurement.getInsideTemp());
+		}
+		Collections.sort(insideTemps);
+		int middle = (insideTemps.size() + 1) / 2;
+
+		return insideTemps.get(middle);
+	}
+
+	public Double getInsideTempMode(ArrayList<Measurement> measurements) {
+		ArrayList<Double> insideTemps = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			insideTemps.add(measurement.getInsideTemp());
+		}
+		int maxFrequenty = 0;
+		double insideTempMode = 0.0;
+		for (int i = 0; i < insideTemps.size(); i++) {
+			int count = 0;
+			for (int j = 0; j < insideTemps.size(); j++) {
+				if (Math.round(insideTemps.get(i)) == Math.round(insideTemps.get(j))) {
+					count++;
+				}
+
+				if (count > maxFrequenty) {
+					maxFrequenty = count;
+					insideTempMode = Math.round(insideTemps.get(i));
+				}
+			}
+		}
+		return insideTempMode;
+	}
+
+	public Double getInsideTempSD(ArrayList<Measurement> measurements) {
+		ArrayList<Double> insideTemps = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			insideTemps.add(measurement.getInsideTemp());
+		}
+		double dividend = 0.0;
+		double averageInsideTemp = this.getAverageInsideTemp(measurements);
+		for (Double insideTemp : insideTemps) {
+			dividend += (insideTemp - averageInsideTemp) * (insideTemp - averageInsideTemp);
+		}
+		return Math.sqrt(dividend/(insideTemps.size()-1));
+	}
+
+	/**
+	 OutsideTemp
+	 */
+	public Double getLowestOutsideTemp(ArrayList<Measurement> measurements) {
+		Measurement first = measurements.get(0);
+		double lowest = first.getOutsideTemp();
+		for (Measurement measurement : measurements) {
+			if (measurement.getOutsideTemp() < lowest) {
+				lowest = measurement.getOutsideTemp();
+			}
+		}
+		return lowest;
+	}
+
+	public Double getHighestOutsideTemp(ArrayList<Measurement> measurements) {
+		Measurement first = measurements.get(0);
+		double highest = first.getOutsideTemp();
+		for (Measurement measurement : measurements) {
+			if (measurement.getOutsideTemp() > highest) {
+				highest = measurement.getOutsideTemp();
+			}
+		}
+		return highest;
+	}
+
+	public Double getAverageOutsideTemp(ArrayList<Measurement> measurements) {
+		double sum = 0;
+		for (Measurement measurement : measurements) {
+			sum += measurement.getOutsideTemp();
+		}
+		return sum / measurements.size();
+	}
+
+	public Double getOutsideTempMedian(ArrayList<Measurement> measurements) {
+		ArrayList<Double> outsideTemps = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			outsideTemps.add(measurement.getOutsideTemp());
+		}
+		Collections.sort(outsideTemps);
+		int middle = (outsideTemps.size() + 1) / 2;
+
+		return outsideTemps.get(middle);
+	}
+
+	public Double getOutsideTempMode(ArrayList<Measurement> measurements) {
+		ArrayList<Double> outsideTemps = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			outsideTemps.add(measurement.getOutsideTemp());
+		}
+		int maxFrequenty = 0;
+		double outsideTempMode = 0.0;
+		for (int i = 0; i < outsideTemps.size(); i++) {
+			int count = 0;
+			for (int j = 0; j < outsideTemps.size(); j++) {
+				if (Math.round(outsideTemps.get(i)) == Math.round(outsideTemps.get(j))) {
+					count++;
+				}
+
+				if (count > maxFrequenty) {
+					maxFrequenty = count;
+					outsideTempMode = Math.round(outsideTemps.get(i));
+				}
+			}
+		}
+		return outsideTempMode;
+	}
+
+	public Double getOutsideTempSD(ArrayList<Measurement> measurements) {
+		ArrayList<Double> outsideTemps = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			outsideTemps.add(measurement.getOutsideTemp());
+		}
+		double dividend = 0.0;
+		double averageOutsideTemp = this.getAverageOutsideTemp(measurements);
+		for (Double outsideTemp : outsideTemps) {
+			dividend += (outsideTemp - averageOutsideTemp) * (outsideTemp - averageOutsideTemp);
+		}
+		return Math.sqrt(dividend/(outsideTemps.size()-1));
+	}
+
+	/**
+	 Barometer
+	 */
+	public Double getLowestBarometer(ArrayList<Measurement> measurements) {
+		Measurement first = measurements.get(0);
+		double lowest = first.getBarometer();
+		for (Measurement measurement : measurements) {
+			if (measurement.getBarometer() < lowest) {
+				lowest = measurement.getBarometer();
+			}
+		}
+		return lowest;
+	}
+
+	public Double getHighestBarometer(ArrayList<Measurement> measurements) {
+		Measurement first = measurements.get(0);
+		double highest = first.getBarometer();
+		for (Measurement measurement : measurements) {
+			if (measurement.getBarometer() > highest) {
+				highest = measurement.getBarometer();
+			}
+		}
+		return highest;
+	}
+
+	public Double getAverageBarometer(ArrayList<Measurement> measurements) {
+		double sum = 0;
+		for (Measurement measurement : measurements) {
+			sum += measurement.getBarometer();
+		}
+		return sum / measurements.size();
+	}
+
+	public Double getBarometerMedian(ArrayList<Measurement> measurements) {
+		ArrayList<Double> baros = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			baros.add(measurement.getBarometer());
+		}
+		Collections.sort(baros);
+		int middle = (baros.size() + 1) / 2;
+
+		return baros.get(middle);
+	}
+
+	public Double getBarometerMode(ArrayList<Measurement> measurements) {
+		ArrayList<Double> Barometer = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			Barometer.add(measurement.getBarometer());
+		}
+		int maxFrequenty = 0;
+		double barometerMode = 0.0;
+		for (int i = 0; i < Barometer.size(); i++) {
+			int count = 0;
+			for (int j = 0; j < Barometer.size(); j++) {
+				if (Math.round(Barometer.get(i)) == Math.round(Barometer.get(j))) {
+					count++;
+				}
+
+				if (count > maxFrequenty) {
+					maxFrequenty = count;
+					barometerMode = Math.round(Barometer.get(i));
+				}
+			}
+		}
+		return barometerMode;
+	}
+
+	public Double getBarometerSD(ArrayList<Measurement> measurements) {
+		ArrayList<Double> Barometers = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			Barometers.add(measurement.getBarometer());
+		}
+		double dividend = 0.0;
+		double averageBarometer = this.getAverageBarometer(measurements);
+		for (Double barometer : Barometers) {
+			dividend += (barometer - averageBarometer) * (barometer - averageBarometer);
+		}
+		return Math.sqrt(dividend/(Barometers.size()-1));
+	}
+
+	/**
+	 Humidity
+	 */
+	public Integer getLowestOutsideHumidity(ArrayList<Measurement> measurements) {
+		Measurement first = measurements.get(0);
+		int lowest = first.getOutsideHum();
+		for (Measurement measurement : measurements) {
+			if (measurement.getOutsideHum() < lowest) {
+				lowest = measurement.getOutsideHum();
+			}
+		}
+		return lowest;
+	}
+
+	public Integer getHighestOutsideHumidity(ArrayList<Measurement> measurements) {
+		Measurement first = measurements.get(0);
+		int highest = first.getOutsideHum();
+		for (Measurement measurement : measurements) {
+			if (measurement.getOutsideHum() > highest) {
+				highest = measurement.getOutsideHum();
+			}
+		}
+		return highest;
+	}
+
+	public Double getAverageOutsideHumidity(ArrayList<Measurement> measurements) {
+		int sum = 0;
+		for (Measurement measurement : measurements) {
+			sum += measurement.getOutsideHum();
+		}
+		return ((1.0*sum) / measurements.size());
+	}
+
+	public Integer getOutsideHumidityMedian(ArrayList<Measurement> measurements) {
+		ArrayList<Integer> hums = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			hums.add(measurement.getOutsideHum());
+		}
+		Collections.sort(hums);
+		int middle = (hums.size() + 1) / 2;
+
+		return hums.get(middle);
+	}
+
+	public Double getOutsideHumidityMode(ArrayList<Measurement> measurements) {
+		ArrayList<Integer> Humidities = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			Humidities.add(measurement.getOutsideHum());
+		}
+		int maxFrequenty = 0;
+		double humidityMode = 0.0;
+		for (int i = 0; i < Humidities.size(); i++) {
+			int count = 0;
+			for (int j = 0; j < Humidities.size(); j++) {
+				if (Humidities.get(i) == Humidities.get(j)) {
+					count++;
+				}
+
+				if (count > maxFrequenty) {
+					maxFrequenty = count;
+					humidityMode = Humidities.get(i);
+				}
+			}
+		}
+		return humidityMode;
+	}
+
+	public Double getOutsideHumiditySD(ArrayList<Measurement> measurements) {
+		ArrayList<Integer> Humidities = new ArrayList<>();
+		for (Measurement measurement : measurements) {
+			Humidities.add(measurement.getOutsideHum());
+		}
+		double dividend = 0.0;
+		double averageOutsideHumidity = this.getAverageOutsideHumidity(measurements);
+		for (int humidity : Humidities) {
+			dividend += (humidity - averageOutsideHumidity) * (humidity - averageOutsideHumidity);
+		}
+		return Math.sqrt(dividend/(Humidities.size()-1));
+	}
 
 
+	/**
+	 * Individueel
+	 */
+	public ArrayList<Measurement> getMeasurements(LocalDate beginPeriod, LocalDate endPeriod){
+		DatabaseConnection db = new DatabaseConnection();
+		ArrayList<RawMeasurement> rawMeasurements = db.getMeasurementsBetween(beginPeriod.atStartOfDay(), endPeriod.atStartOfDay());
+		ArrayList<Measurement> measurements = new ArrayList<>();
+		for (RawMeasurement rawMeasurement : rawMeasurements){
+			Measurement measurement = new Measurement(rawMeasurement);
+			measurements.add(measurement);
+		}
+		return measurements;
+	}
+
+	public double getDiffWindChillAndOutsideTemp(LocalDate beginPeriod, LocalDate endPeriod){
+		ArrayList<Measurement> measurements = getMeasurements(beginPeriod, endPeriod);
+		double biggestDifference = 0;
+		for (Measurement measurement : measurements){
+			double outsideTemp = measurement.getOutsideTemp();
+			// Overflow Check
+			if (outsideTemp > 1000 || outsideTemp < -1000){
+				continue;
+			}
+			double windChill = measurement.getWindChill();
+			// Overflow Check
+			if (windChill > 1000 || windChill < -1000){
+				continue;
+			}
+			double biggestDiff = windChill - outsideTemp;
+			double biggestDiff2 = outsideTemp - windChill;
+			if (biggestDiff > 0 && biggestDiff2 <= 0 && biggestDiff > biggestDifference){
+				biggestDifference = biggestDiff;
+			} else if (biggestDiff2 > 0 && biggestDiff <= 0 && biggestDiff2 > biggestDifference){
+				biggestDifference = biggestDiff2;
+			}
+		}
+		return biggestDifference;
+	}
+
+	public LocalDate getDateBiggestDiff(LocalDate beginPeriod, LocalDate endPeriod){
+		double biggestDiff = getDiffWindChillAndOutsideTemp(beginPeriod, endPeriod);
+		ArrayList<Measurement> measurements = getMeasurements(beginPeriod, endPeriod);
+		LocalDate theDate = null;
+		for (Measurement measurement : measurements){
+			LocalDate date = LocalDate.from(measurement.getDateStamp());
+			if ((measurement.getOutsideTemp() - measurement.getWindChill()) > 0){
+				if ((measurement.getOutsideTemp() - measurement.getWindChill()) == biggestDiff){
+					theDate = date;
+				}
+			} else if ((measurement.getWindChill() - measurement.getOutsideTemp()) > 0){
+				if ((measurement.getWindChill() - measurement.getOutsideTemp()) == biggestDiff){
+					theDate = date;
+				}
+			}
+		}
+		return theDate;
+	}
 }
