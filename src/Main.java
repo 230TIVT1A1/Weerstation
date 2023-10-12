91b2bc6e0b49674b162cf16bd362dc45bc906fbb
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2023-10-12 13:09:31 +0200

    removed duplicate code

--- src/Main.java
+++ src/Main.java
@@ -16,60 +16,6 @@ public class Main {
         period.longestRainFall();
         period.maxTemperatureDifference();
         /**
-         Mediaan
-         */
-        System.out.println("Mediaan Binnentemperatuur: " + period.getInsideTempMedian());
-        System.out.println("Mediaan Buitentemperatuur: " + period.getOutsideTempMedian());
-        System.out.println("Mediaan Barometer: " + period.getBarometerMedian());
-        System.out.println("Mediaan Luchtvochtigheid Buiten: " + period.getOutsideHumidityMedian());
-
-        System.out.println("---------------------------------------------");
-        /**
-         Laagste
-         */
-        System.out.println("Laagste Binnentemperatuur: " + period.getLowestInsideTemp());
-        System.out.println("Laagste Buitentemperatuur: " + period.getLowestOutsideTemp());
-        System.out.println("Laagste Barometer: " + period.getLowestBarometer());
-        System.out.println("Laagste Luchtvochtigheid Buiten: " + period.getLowestOutsideHumidity());
-
-        System.out.println("---------------------------------------------");
-        /**
-         Hoogste
-         */
-        System.out.println("Hoogste Binnentemperatuur: " + period.getHighestInsideTemp());
-        System.out.println("Hoogste Buitentemperatuur: " + period.getHighestOutsideTemp());
-        System.out.println("Hoogste Barometer: " + period.getHighestBarometer());
-        System.out.println("Hoogste Luchtvochtigheid Buiten: " + period.getHighestOutsideHumidity());
-
-        System.out.println("---------------------------------------------");
-        /**
-         Gemiddelde
-         */
-        System.out.println("Gemiddelde Binnentemperatuur: " + period.getAverageInsideTemp());
-        System.out.println("Gemiddelde Buitentemperatuur: " + period.getAverageOutsideTemp());
-        System.out.println("Gemiddelde Barometer: " + period.getAverageBarometer());
-        System.out.println("Gemiddelde Luchtvochtigheid Buiten: " + period.getAverageOutsideHumidity());
-
-        System.out.println("---------------------------------------------");
-        /**
-         Mediaan
-         */
-        System.out.println("Mediaan Binnentemperatuur: " + period.getInsideTempMedian());
-        System.out.println("Mediaan Buitentemperatuur: " + period.getOutsideTempMedian());
-        System.out.println("Mediaan Barometer: " + period.getBarometerMedian());
-        System.out.println("Mediaan Luchtvochtigheid Buiten: " + period.getOutsideHumidityMedian());
-
-        System.out.println("---------------------------------------------");
-        /**
-         Mediaan
-         */
-        System.out.println("Mediaan Binnentemperatuur: " + period.getInsideTempMedian(period.getMeasurements()));
-        System.out.println("Mediaan Buitentemperatuur: " + period.getOutsideTempMedian(period.getMeasurements()));
-        System.out.println("Mediaan Barometer: " + period.getBarometerMedian(period.getMeasurements()));
-        System.out.println("Mediaan Luchtvochtigheid Buiten: " + period.getOutsideHumidityMedian(period.getMeasurements()));
-
-        System.out.println("---------------------------------------------");
-        /**
          Laagste
          */
         System.out.println("Laagste Binnentemperatuur: " + period.getLowestInsideTemp(period.getMeasurements()));
