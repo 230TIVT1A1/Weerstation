d8f7d8eab6854dadcdfe6c551b862ffdf778578b
Doppie <eriksszon@gmail.com>
2023-10-12 18:28:54 +0200

    Made changes to the icons

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,9 @@ public class Main {
         System.out.println(measurement.getOutsideHum());
         System.out.println(measurement.getInsideTemp());
         IO.init();
-        GUI.drawPicture("/Icons/thermostat.png",1,2);
+        GUI.drawPicture("/Icons/thermostat.png",1,1);
+        GUI.drawPicture("/Icons/thermostatSelected.png",16,1);
+//        GUI.drawPicture("/Icons/Clearer.png",0,0);
         GUI.clearSegment();
         GUI.segmentNumber(measurement.getOutsideTemp(),0,"00.00");
         GUI.segmentNumber(measurement.getOutsideHum(),1,"00");
