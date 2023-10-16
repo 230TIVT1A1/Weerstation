b5d6879ffac2c931582cbc58d7be20021eefe25d
Doppie <eriksszon@gmail.com>
2023-10-16 16:07:02 +0200

    added a test method to the Icon klass method

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,12 @@ public class Main {
 //        System.out.println(measurement.getOutsideTemp());
 //        System.out.println(measurement.getOutsideHum());
 //        System.out.println(measurement.getInsideTemp());
-//        IO.init();
-//        GUI.clearSegment();
+        GUI.drawPicture("/Icons/thermostat.png",1,1);
+        GUI.drawPicture("/Icons/thermostatSelected.png",16,1);
+        GUI.drawPicture("/Icons/wind.png",32,1);
+//        GUI.drawPicture("/Icons/Clearer.png",0,0);
+        IO.init();
+        GUI.clearSegment();
 //        GUI.segmentNumber(measurement.getOutsideTemp(),0,"00.00");
 //        GUI.segmentNumber(measurement.getOutsideHum(),1,"00");
 //        GUI.segmentNumber(measurement.getInsideTemp(),2,"0.0");
