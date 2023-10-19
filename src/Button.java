94bea71ed0aa718013df8f89377d510b12ad9c7e
boomkip <mpl.groeneveld@gmail.com>
2023-10-19 15:25:30 +0200

    Edited button class independent methods

--- src/Button.java
+++ src/Button.java
@@ -1,19 +1,14 @@
 public class Button {
-    public static void main(String[] Args) {
-        IO.init();
+
+    public Boolean previousButtonPressed() {
+        return IO.readShort(0x90) != 0;
     }
 
-    public void listenForButtons() {
-        while (true) {
-            if ( IO.readShort(0x80) != 0 ) {
-                // Select functie
-            }
-            if ( IO.readShort(0x90) != 0 ) {
-                // Previous functie
-            }
-            if ( IO.readShort(0x100) != 0 ) {
-                // Next functie
-            }
-        }
+    public Boolean nextButtonPressed() {
+        return IO.readShort(0x100) != 0;
+    }
+
+    public Boolean selectButtonPressed() {
+        return IO.readShort(0x80) != 0;
     }
 }
