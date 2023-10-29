a63000b3c573c358b0f29c909f75244a6d7c1908
Lucas Verhulst <lucasverhulst@hotmail.nl>
2023-10-29 18:38:57 +0100

    sorted icons folder

--- src/Main.java
+++ src/Main.java
@@ -30,37 +30,37 @@ public class Main {
         mainMenu.addItem(new MenuItem("extra","icons/period"));
         mainMenu.addItem(new MenuItem("exit","icons/quit"));
 
-        Menu extraMenu = new Menu("extraMenu", 1, 3);
-        extraMenu.addItem(new MenuItem("back","icons/back"));
-        extraMenu.addItem(new MenuItem("period", "icons/periode"));
-        extraMenu.addItem(new MenuItem("assignments", "icons/assignments"));
-
         Menu subMenu = new Menu("subMenu", 1, 7);
         subMenu.addItem(new MenuItem("back","icons/back"));
-        subMenu.addItem(new MenuItem("modus","icons/modus"));
-        subMenu.addItem(new MenuItem("mediaan","icons/mediaan"));
-        subMenu.addItem(new MenuItem("standaardafwijking","icons/standaardafwijking"));
-        subMenu.addItem(new MenuItem("gemiddeld","icons/gemiddelde"));
-        subMenu.addItem(new MenuItem("minimaal","icons/minimaal"));
-        subMenu.addItem(new MenuItem("maximaal","icons/maximaal"));
+        subMenu.addItem(new MenuItem("modus","icons/submenu/modus"));
+        subMenu.addItem(new MenuItem("mediaan","icons/submenu/mediaan"));
+        subMenu.addItem(new MenuItem("standaardafwijking","icons/submenu/standaardafwijking"));
+        subMenu.addItem(new MenuItem("gemiddeld","icons/submenu/gemiddelde"));
+        subMenu.addItem(new MenuItem("minimaal","icons/submenu/minimaal"));
+        subMenu.addItem(new MenuItem("maximaal","icons/submenu/maximaal"));
+
+        Menu extraMenu = new Menu("extraMenu", 1, 3);
+        extraMenu.addItem(new MenuItem("back","icons/back"));
+        extraMenu.addItem(new MenuItem("period", "icons/extramenu/periode"));
+        extraMenu.addItem(new MenuItem("assignments", "icons/extramenu/assignments"));
 
         Menu periodMenu = new Menu("periodMenu", 1, 7);
         periodMenu.addItem(new MenuItem("back","icons/back"));
-        periodMenu.addItem(new MenuItem("Last year","icons/lastyear"));
-        periodMenu.addItem(new MenuItem("Last month","icons/lastmonth"));
-        periodMenu.addItem(new MenuItem("Last week","icons/lastweek"));
-        periodMenu.addItem(new MenuItem("Last Hour","icons/lasthour"));
-        periodMenu.addItem(new MenuItem("This day","icons/thisday"));
-        periodMenu.addItem(new MenuItem("Yesterday","icons/yesterday"));
+        periodMenu.addItem(new MenuItem("Last year","icons/periodmenu/lastyear"));
+        periodMenu.addItem(new MenuItem("Last month","icons/periodmenu/lastmonth"));
+        periodMenu.addItem(new MenuItem("Last week","icons/periodmenu/lastweek"));
+        periodMenu.addItem(new MenuItem("Last Hour","icons/periodmenu/lasthour"));
+        periodMenu.addItem(new MenuItem("This day","icons/periodmenu/today"));
+        periodMenu.addItem(new MenuItem("Yesterday","icons/periodmenu/yesterday"));
 
         Menu assignmentsMenu = new Menu("assignmentsMenu",1,7);
         assignmentsMenu.addItem(new MenuItem("back","icons/back"));
-        assignmentsMenu.addItem(new MenuItem("Assignment Daan","icons/assignmentdaan"));
-        assignmentsMenu.addItem(new MenuItem("Assignment Storm","icons/assignmentstorm"));
-        assignmentsMenu.addItem(new MenuItem("Assignment Melvin","icons/assignmentmelvin"));
-        assignmentsMenu.addItem(new MenuItem("Assignment Tiemen","icons/assignmenttiemen"));
-        assignmentsMenu.addItem(new MenuItem("Assignment Erik","icons/assignmenterik"));
-        assignmentsMenu.addItem(new MenuItem("Assignment Lucas","icons/assignmentlucas"));
+        assignmentsMenu.addItem(new MenuItem("Assignment Daan","icons/assignment/daan"));
+        assignmentsMenu.addItem(new MenuItem("Assignment Storm","icons/assignment/storm"));
+        assignmentsMenu.addItem(new MenuItem("Assignment Melvin","icons/assignment/melvin"));
+        assignmentsMenu.addItem(new MenuItem("Assignment Tiemen","icons/assignment/tiemen"));
+        assignmentsMenu.addItem(new MenuItem("Assignment Erik","icons/assignment/erik"));
+        assignmentsMenu.addItem(new MenuItem("Assignment Lucas","icons/assignment/lucas"));
 
         ArrayList<Menu> menuArray = new ArrayList<>();
         menuArray.add(mainMenu);
